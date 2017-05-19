aff68d3f8fc0eeee62021bc0544b4a9aad6908f2
cuproPanda <cupropanda@gmail.com>
2017-05-19 15:01:06 -0400

    Finished ScenicBench, FlowerArch changes graphic depending on season/weather

---
+++ Source/ZenGarden/Building_FlowerArch.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace ZenGarden {
+  [StaticConstructorOnStartup]
+  internal class Building_FlowerArch : Building {
+
+    private static Graphic GraphicPlain = GraphicDatabase.Get<Graphic_Single>("Cupro/Object/FlowerArch/FlowerArch_Plain",       ShaderDatabase.DefaultShader, new Vector2(2, 2), Color.white);
+    private static Graphic GraphicVines = GraphicDatabase.Get<Graphic_Single>("Cupro/Object/FlowerArch/FlowerArch_Vines",       ShaderDatabase.DefaultShader, new Vector2(2, 2), Color.white);
+    private static Graphic GraphicBlooming = GraphicDatabase.Get<Graphic_Single>("Cupro/Object/FlowerArch/FlowerArch_Blooming", ShaderDatabase.DefaultShader, new Vector2(2, 2), Color.white);
+    private static Graphic GraphicFrozen = GraphicDatabase.Get<Graphic_Single>("Cupro/Object/FlowerArch/FlowerArch_Frozen",     ShaderDatabase.DefaultShader, new Vector2(2, 2), Color.white);
+
+    public override Graphic Graphic {
+      get {
+        if (Map.weatherManager.SnowRate > 0) {
+          return GraphicFrozen;
+        }
+        if (Temperature > 5f) {
+          if (Season == Season.Spring || Season == Season.Summer) {
+            return GraphicBlooming;
+          }
+          return GraphicVines;
+        }
+        return GraphicPlain;
+      }
+    }
+
+    public float Temperature {
+      get {
+        float num;
+        if (!GenTemperature.TryGetTemperatureForCell(Position, Map, out num)) {
+          return 1f;
+        }
+        return num;
+      }
+    }
+
+    private Season Season {
+      get {
+        return GenLocalDate.Season(Map);
+      }
+    }
+  }
+}
--- Source/ZenGarden/JobDriver_SitAtScenicBench.cs
+++ Source/ZenGarden/JobDriver_SitAtScenicBench.cs
@@ -21,38 +21,47 @@ namespace ZenGarden {
       // Reference the tick manager so Find isn't constantly called
       TickManager tickMan = Find.TickManager;
 
-      // Create a new cellRect to get random look spots
-      CellRect cellRect = new CellRect() {
-        minX = (TargetA.Cell.x - 5),
-        maxX = (TargetA.Cell.x + 5),
-        minZ = (TargetA.Cell.z - 5),
-        maxZ = (TargetA.Cell.z + 5),
-      };
-
       // Reserve the bench, go to it, then sit down
       yield return Toils_Reserve.Reserve(BenchInd);
       yield return Toils_Goto.GotoCell(BenchInd, PathEndMode.OnCell);
 
-      // Get surrounding beauty
-      surroundingBeauty = JoyFromSurroundings();
-
       // Set up the toil
       Toil sitAtBench = new Toil();
-      JoyKindDef joyKind = DefDatabase<JobDef>.GetNamed("ZEN_SitAtScenicBench").joyKind;
+      JobDef joyJob = DefDatabase<JobDef>.GetNamed("ZEN_SitAtScenicBench");
       sitAtBench.socialMode = RandomSocialMode.Normal;
+      sitAtBench.initAction = () => { surroundingBeauty = BeautyUtility.AverageBeautyPerceptible(pawn.Position, Map); };
       sitAtBench.tickAction = () => {
-        // TODO: Test this value, max joy per hour is 30
-        pawn.needs.joy.GainJoy(Mathf.Min(surroundingBeauty * 0.0004f, 0.012f), joyKind);
+        pawn.needs.joy.GainJoy(joyJob.joyGainRate * 0.000144f, joyJob.joyKind);
+        pawn.needs.joy.GainJoy(Mathf.Min(Mathf.Max(surroundingBeauty / 2f, 0.3f), 2.5f) * 0.000144f, joyJob.joyKind);
+        // Gain comfort from sitting on the bench
+        pawn.GainComfortFromCellIfPossible();
 
         // Occasionally look in a different direction, observing the surroundings
         if (tickMan.TicksGame % 250 == 0) {
-          pawn.Drawer.rotator.FaceCell(cellRect.RandomCell);
+          pawn.Drawer.rotator.FaceCell(GenAdj.RandomAdjacentCellCardinal(pawn.Position));
+        }
+
+        // End this job once the pawn has maxed out their joy
+        if (pawn.needs.joy.CurLevel > 0.9999f) {
+          pawn.jobs.curDriver.EndJobWith(JobCondition.Succeeded);
         }
       };
       sitAtBench.defaultCompleteMode = ToilCompleteMode.Delay;
       sitAtBench.defaultDuration = CurJob.def.joyDuration;
       sitAtBench.AddFinishAction(() => {
-        Thought_Memory memory = (Thought_Memory)ThoughtMaker.MakeThought(ThoughtDef.Named("ZEN_EnjoyedScenery"));
+        Thought_Memory memory;
+        // If the scenery was very beautiful, give a better memory,
+        if (surroundingBeauty > 5f) {
+          memory = (Thought_Memory)ThoughtMaker.MakeThought(ThoughtDef.Named("ZEN_EnjoyedBeautifulScenery")); 
+        }
+        // or if the scenery was okay, give a normal memory,
+        else if (surroundingBeauty > 0) {
+          memory = (Thought_Memory)ThoughtMaker.MakeThought(ThoughtDef.Named("ZEN_EnjoyedScenery"));
+        }
+        // otherwise, give a bad memory
+        else {
+          memory = (Thought_Memory)ThoughtMaker.MakeThought(ThoughtDef.Named("ZEN_DidNotEnjoyScenery"));
+        }
         pawn.needs.mood.thoughts.memories.TryGainMemory(memory);
       });
 
@@ -61,18 +70,23 @@ namespace ZenGarden {
     }
 
 
-    private float JoyFromSurroundings() {
-      Thing bench = TargetA.Thing;
-      float beauty = 0;
-
-      RegionTraverser.BreadthFirstTraverse(bench.GetRegion(), (Region from, Region r) => r.portal == null, delegate (Region r) {
-        foreach (IntVec3 current in r.Cells) {
-          if (current.InHorDistOf(bench.Position, 15f)) {
-            beauty += BeautyUtility.CellBeauty(current, Map);
-          }
+    private float AverageBeautyPerceptible(IntVec3 root, Map map) {
+      List<Thing> tempCountedThings = new List<Thing>();
+      float beauty = 0f;
+      int cells = 0;
+      BeautyUtility.FillBeautyRelevantCells(root, map);
+      for (int i = 0; i < BeautyUtility.beautyRelevantCells.Count; i++) {
+        // Get the beauty for this cell
+        beauty += BeautyUtility.CellBeauty(BeautyUtility.beautyRelevantCells[i], map, tempCountedThings);
+        // Add +1 beauty for any plant that doesn't already have beauty
+        Plant plant = BeautyUtility.beautyRelevantCells[i].GetPlant(Map);
+        if (plant != null && plant.GetStatValue(StatDefOf.Beauty) <= 0) {
+          beauty += 1f;
         }
-        return false;
-      });
+        cells++;
+      }
+      // Divide beauty by the number of cells scanned to get the average
+      beauty /= cells;
       return beauty;
     }
   }
