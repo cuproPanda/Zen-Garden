0cf3d465632ce0766822f204e17b9611d548cc7d
cuproPanda <cupropanda@gmail.com>
2017-06-10 00:24:03 -0400

    patched architect sense, cherries grow in spring and summer

---
+++ Source/ZenGarden/PatchOperationModDependent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+using Verse;
+
+namespace ZenGarden {
+
+  internal class PatchOperationModDependent : PatchOperation {
+
+    private List<string> modList;
+    private string modName;
+
+    protected override bool ApplyWorker(XmlDocument xml) {
+
+      if (!modList.NullOrEmpty()) {
+        return ApplyWorker_Multiple();
+      }
+
+      if (modName.NullOrEmpty()) {
+        return false;
+      }
+
+      return ApplyWorker_Single();
+    }
+
+
+    private bool ApplyWorker_Multiple() {
+      for (int m = 0; m < modList.Count; m++) {
+        if (!ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name == modList[m])) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+
+    private bool ApplyWorker_Single() {
+      return ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name == modName);
+    }
+  }
+}
