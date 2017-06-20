a71b4ea5d7f31ebb0015fe69883fe957ce5182af
cuproPanda <cupropanda@gmail.com>
2017-06-20 17:41:41 -0400

    Fountain supports more frames thanks to mrofa

--- Source/ZenGarden/Building_Fountain.cs
+++ Source/ZenGarden/Building_Fountain.cs
@@ -1,40 +1,70 @@
-﻿using RimWorld;
+﻿using System.Linq;
+
+using UnityEngine;
+using RimWorld;
 using Verse;
 
 namespace ZenGarden {
   [StaticConstructorOnStartup]
   public class Building_Fountain : Building {
 
+    public static Graphic[] AnimFrames;
+    public CompPowerTrader powerComp;
+    public Graphic animOff;
+
     private TickManager tickMan;
-    private CompPowerTrader powerComp;
+    private Graphic currFrame;
+    private int frameLerp = 0;
+    private int FrameCount = 4;
 
 
     public override void SpawnSetup(Map map, bool respawningAfterLoad) {
       base.SpawnSetup(map, respawningAfterLoad);
       tickMan = Find.TickManager;
       powerComp = GetComp<CompPowerTrader>();
-      Rotation = Rot4.North;
+      LongEventHandler.ExecuteWhenFinished(GetGraphicArray);
+    }
+
+
+    public void GetGraphicArray() {
+      //Creating frame array
+      AnimFrames = new Graphic_Single[FrameCount];
+      for (int i = 0; i < FrameCount; i++) {
+        AnimFrames[i] = GraphicDatabase.Get<Graphic_Single>("Cupro/Anim/FountainWater" + (i + 1), ShaderDatabase.Transparent, def.graphicData.drawSize, new Color(1f, 1f, 1f, 0.65f));
+      }
+      if (AnimFrames.Count() > 0) {
+        currFrame = AnimFrames.FirstOrDefault();
+      }
+      animOff = GraphicDatabase.Get<Graphic_Single>("Cupro/Anim/FountainWater0", ShaderDatabase.Transparent, def.graphicData.drawSize, new Color(1f, 1f, 1f, 0.65f));
     }
 
 
     public override void Tick() {
       base.Tick();
-      if (tickMan.TicksGame % 50 == 0) {
-        if (powerComp.PowerOn) {
-          if (Rotation == Rot4.North) {
-            Rotation = Rot4.East;
-          }
-          else if (Rotation == Rot4.East) {
-            Rotation = Rot4.South;
-          }
-          else if (Rotation == Rot4.South) {
-            Rotation = Rot4.East;
-          }
+
+      if (tickMan.TicksGame % 35 == 0 && powerComp.PowerOn) {
+        frameLerp++;
+        if (frameLerp >= FrameCount) {
+          frameLerp = 0;
+        }
+        currFrame = AnimFrames[frameLerp];
+      }
+    }
+
+
+    public override void Draw() {
+      base.Draw();
+      if (currFrame != null && animOff != null) {
+        Matrix4x4 matrix = default(Matrix4x4);
+        Vector3 s = new Vector3(def.graphicData.drawSize.x, 9f, def.graphicData.drawSize.y); //Size and altitude
+        Vector3 x = new Vector3(0f, 0f, 0f); // This is a offset for drawing position
+        matrix.SetTRS(DrawPos + x + Altitudes.AltIncVect, Rotation.AsQuat, s);
+        if (powerComp.PowerOn && AnimFrames.Count() > 0) {
+          Graphics.DrawMesh(MeshPool.plane10, matrix, currFrame.MatAt(Rotation), 0);
         }
         else {
-          Rotation = Rot4.North;
+          Graphics.DrawMesh(MeshPool.plane10, matrix, animOff.MatAt(Rotation), 0);
         }
-        Map.mapDrawer.MapMeshDirty(Position, MapMeshFlag.Things);
       }
     }
   }
